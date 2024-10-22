48aa38638701f8dd208356297fccd66ff28691a7
MarikaBBB <mbertelli012@gmail.com>
2024-10-22 23:14:45 +0100

    challenge 4 attempt to complete

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,15 +1,45 @@
 const { Layout } = require("../templates.js");
+const { getSession } = require("../model/session");
 
 function get(req, res) {
-  /**
-   * [1] Read session ID from cookie
-   * [2] Get session from DB
-   * [3] If the session exists render a log out form
-   * [4] This should submit a request to `POST /log-out`
-   * [5] Else render the sign up/log in links
-   */
-  const title = "Confess your secrets!";
-  const content = /*html*/ `
+    // [1] Read session ID from signed cookie
+    const sid = req.signedCookies.sid;
+    let content;
+  
+    if (sid) {
+      // [2] Get session from DB
+      const session = getSession(sid);
+  
+      // [3] If the session exists, render a log out form
+      if (session) {
+        content = `
+          <div class="Cover">
+            <h1>Welcome back!</h1>
+            <form method="POST" action="/log-out">
+              <button class="Button">Log out</button>
+            </form>
+          </div>
+        `;
+      } else {
+        // If no session, show login/signup links
+        content = `
+          <div class="Cover">
+            <h1>Confess your secrets!</h1>
+            <nav><a href="/sign-up">Sign up</a> or <a href="/log-in">log in</a></nav>
+          </div>
+        `;
+      }
+    } else {
+      // [5] If no session ID, show login/signup links
+      content = `
+        <div class="Cover">
+          <h1>Confess your secrets!</h1>
+          <nav><a href="/sign-up">Sign up</a> or <a href="/log-in">log in</a></nav>
+        </div>
+      `;
+    }
+   
+  const title = "Confess your secrets!";`
     <div class="Cover">
       <h1>${title}</h1>
       <nav><a href="/sign-up">Sign up</a> or <a href="/log-in">log in</a></nav>
--- src/routes/log-out.js
+++ src/routes/log-out.js
@@ -1,11 +1,20 @@
+const { removeSession } = require('../model/session'); 
+
 function post(req, res) {
-  /**
-   * [1] Get the session ID from the cookie
-   * [2] Remove that session from the DB
-   * [3] Remove the session cookie
-   * [4] Redirect back home
-   */
-  res.status(500).send("");
+  // [1] Get the session ID from the signed cookie
+  const sid = req.signedCookies.sid;
+
+  if (sid) {
+    // [2] Remove that session from the DB
+    removeSession(sid);
+
+    // [3] Remove the session cookie
+    res.clearCookie('sid',{ signed: true, httpOnly: true });
+  }
+
+  // [4] Redirect back to the homepage
+  res.redirect('/');
+
 }
 
 module.exports = { post };
